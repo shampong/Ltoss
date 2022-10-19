dd1578fc2037e6f729057df1d1154f9f12666b76
jjy <junyung0721@naver.com>
2022-10-19 16:39:56 +0900

    Display panel Materials Analysis first commit

---
+++ src/test/java/ltoss/dma/DmaApplicationTests.java
@@ -0,0 +1,13 @@
+package ltoss.dma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
